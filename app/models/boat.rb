a45d3d8aaaf88a87283e849f48163dc00758c286
chsohn15 <chsohn15@gmail.com>
2020-07-26 20:01:35 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
@@ -5,26 +6,32 @@ class Boat < ActiveRecord::Base
 
   def self.first_five
     # all.limit(5)
+    Boat.all.limit(5)
   end
 
   def self.dinghy
     # where("length < 20")
+    Boat.all.where("length < 20")
   end
 
   def self.ship
     # where("length >= 20")
+    Boat.all.where("length >= 20")
   end
 
   def self.last_three_alphabetically
     # all.order(name: :desc).limit(3)
+    Boat.all.order(name: :desc).limit(3)
   end
 
   def self.without_a_captain
     # where(captain_id: nil)
+    Boat.all.where(captain_id: nil)
   end
 
   def self.sailboats
     # includes(:classifications).where(classifications: { name: 'Sailboat' })
+    Boat.all.includes(:classifications).where(classifications: { name: 'Sailboat' })
   end
 
   def self.with_three_classifications
@@ -32,7 +39,8 @@ class Boat < ActiveRecord::Base
     # regularly. Just know that we can get this out of the database in
     # milliseconds whereas it would take whole seconds for Ruby to do the same.
     #
-    # joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
+    # 
+    Boat.all.joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
   end
 
   def self.non_sailboats
@@ -40,6 +48,6 @@ class Boat < ActiveRecord::Base
   end
 
   def self.longest
-    # order('length DESC').first
+    Boat.all.order('length DESC').first
   end
 end
